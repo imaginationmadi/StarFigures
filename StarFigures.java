b86abfb0f35a803752a30ac32b7df40e88e8cfde
imaginationmadi <145612004+imaginationmadi@users.noreply.github.com>
2023-09-30 14:38:41 -0700

    StarFigures
    Prints our three different shapes using stars ( * ).

---
+++ StarFigures.java
@@ -0,0 +1,15 @@
+// Chapter 1 StarFigures
+
+public class StarFigures {
+
+	public static void main (String[] args) {
+		
+		System.out.println ("*****\n*****\n * *\n  *\n * *");
+		System.out.println ();
+		System.out.println ("*****\n*****\n * *\n  *\n * *\n*****\n*****");
+		System.out.println ();
+		System.out.println ("  *\n  *\n  *\n*****\n*****\n * *\n  *\n * *");
+		
+	}
+
+}
